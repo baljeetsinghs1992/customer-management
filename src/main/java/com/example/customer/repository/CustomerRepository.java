34481f1c4ab15bdd82be6a39f068a50e520a9e37
baljeetsinghs1992 <baljeetsinghs1992@gmail.com>
2025-04-29 20:31:47 +0530

    Update CustomerRepository.java

--- src/main/java/com/example/customer/repository/CustomerRepository.java
+++ src/main/java/com/example/customer/repository/CustomerRepository.java
@@ -3,8 +3,10 @@ package com.example.customer.repository;
 import com.example.customer.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
-
+    Optional<Customer> findByName(String name);
+    Optional<Customer> findByEmail(String email);
 }
